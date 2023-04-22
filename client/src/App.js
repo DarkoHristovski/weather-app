f853a13a2ca2e7bb5c579ff39f593586e43fdb30
DarkoH <hristovski15@gmail.com>
2023-04-23 00:41:39 +0200

    new_test_commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import {useState, useEffect} from 'react'
 
 
 function App() {
+const [longitude, setlongitude]= useState(null)
+const [latitude, setlatitude]= useState(null)
+
+useEffect(()=>{
+  const cordinatesApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={frankfurt}&appid=627f2f8c91e540f7514a66b15560a3b3';
+  fetch(cordinatesApiUrl)
+  .then(res => res.json())
+  .then(results =>{
+    setlongitude(results[0].lon)
+    setlatitude(results[0].lat);
+  })
+},[])
+
+
+
+console.log(longitude)
+console.log(latitude)
+
   
 
 
