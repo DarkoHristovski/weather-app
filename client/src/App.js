888b080847c55ca1b4df926d94faf9b898abdab7
DarkoH <hristovski15@gmail.com>
2023-04-24 22:54:16 +0200

    new functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
+
 import {useState, useEffect} from 'react'
+import Form from "./components/UI/Form";
 
 
 function App() {
 const [longitude, setlongitude]= useState(null)
 const [latitude, setlatitude]= useState(null)
 
+const apiKey= '627f2f8c91e540f7514a66b15560a3b3';
+const cordinatesApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={frankfurt}&appid=627f2f8c91e540f7514a66b15560a3b3';
+
+const currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+const hourlyWeatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+
+
+
 useEffect(()=>{
-  const cordinatesApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={frankfurt}&appid=627f2f8c91e540f7514a66b15560a3b3';
   fetch(cordinatesApiUrl)
   .then(res => res.json())
   .then(results =>{
@@ -21,11 +30,17 @@ useEffect(()=>{
 console.log(longitude)
 console.log(latitude)
 
+console.log(currentWeatherApi)
+
+console.log(hourlyWeatherApi)
+
   
 
 
   return (
-    <div></div>
+    <div>
+      <Form/>
+    </div>
   );
 }
 
