763962158d8f31371ef59a545c799cee370a4852
DarkoH <hristovski15@gmail.com>
2023-04-24 23:03:53 +0200

    resolving

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import {useState, useEffect} from 'react'
-
-import {useState, useEffect} from 'react'
 import Form from "./components/UI/Form";
 
 
 function App() {
 const [longitude, setlongitude]= useState(null)
 const [latitude, setlatitude]= useState(null)
-<<<<<<< HEAD
-=======
+
 
 const apiKey= '627f2f8c91e540f7514a66b15560a3b3';
 const cordinatesApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={frankfurt}&appid=627f2f8c91e540f7514a66b15560a3b3';
@@ -37,9 +34,6 @@ console.log(currentWeatherApi)
 
 console.log(hourlyWeatherApi)
 
-  
->>>>>>> app-api
-
 useEffect(()=>{
   const cordinatesApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={skopje}&appid=627f2f8c91e540f7514a66b15560a3b3';
   fetch(cordinatesApiUrl)
@@ -50,23 +44,13 @@ useEffect(()=>{
   })
 },[])
 
-<<<<<<< HEAD
 
-
-console.log(longitude)
-console.log(latitude)
-
-
-return (
-  <div></div>
-);
-=======
   return (
     <div>
       <Form/>
     </div>
   );
->>>>>>> app-api
+
 }
 
 export default App;
