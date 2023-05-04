0022ab8a3b93d0b6bd866e24cce055331fe79e23
DarkoH <hristovski15@gmail.com>
2023-05-04 13:50:07 +0200

    adding new icon and new strucutre of the site

--- client/src/components/WeatherHiglits.js
+++ client/src/components/WeatherHiglits.js
@@ -1,54 +1,63 @@
 import Card from "./UI/Card";
 import styles from "../../src/global.module.css";
-import classes from './weather-highlihts.module.css';
-import windIcon from './../icons/wind-icon.svg';
-
-const WeatherHighlihts = (props) =>{
-    let minTempToCelsius = Math.floor(props.cityData.main.temp_min - 273,15);
-   let maxTempToCelsius = Math.floor(props.cityData.main.temp_max - 273,15);
-   let feelsLikeTempToCelsius = Math.floor(props.cityData.main.feels_like - 273,15);
+import classes from "./weather-highlihts.module.css";
+import windIcon from "./../icons/wind-icon.svg";
+import humidityIcon from "./../icons/humidity-icon.svg";
+import sunsetIcon from "./../icons/sunset-icon.svg";
+import sunriseIcon from "./../icons/sunrise-icon.svg";
 
 
-   let sunrise = props.cityData.sys.sunrise
-   let sunriseHour = new Date(sunrise*1000);
-   let sunset = props.cityData.sys.sunset
-   let sunsetHour = new Date(sunset*1000);
-
-  
-   sunriseHour.toLocaleTimeString('en-US');
-   // expected output: 1:15:30 AM
 
 
-    return (
-    <div className={styles['card-content']}>
-    <p>Today Highlihts</p>
-    <div className={styles['display-flex']}>
-        <div className={classes.block}>
+
+const WeatherHighlihts = (props) => {
+  let sunrise = props.cityData.sys.sunrise;
+  let sunriseHour = new Date(sunrise * 1000);
+  let sunset = props.cityData.sys.sunset;
+  let sunsetHour = new Date(sunset * 1000);
+
+  sunriseHour.toLocaleTimeString("en-US");
+  // expected output: 1:15:30 AM
+
+  return (
+    <div className={styles["card-content"]}>
+      <p>Today Highlihts</p>
+      <div className={styles["display-flex"]}>
         <Card>
-        <p>Wind</p>
-        <img src={windIcon} alt="" />
-        <p>{props.cityData.wind.speed} km/h</p>
+          <p>Wind</p>
+          <div className={classes["img-wrapper"]}>
+            <img src={windIcon} alt="" />
+          </div>
+          <p>{props.cityData.wind.speed} km/h</p>
         </Card>
-        <Card><p>Humidity: {props.cityData.main.humidity} %</p></Card>
-        </div>
-        <div className={classes.block}>
-        <Card><p>Min: {minTempToCelsius}&#x2103; Max: {maxTempToCelsius}&#x2103; </p>
-    <p>Main: {props.cityData.weather[0].main}</p></Card>
-        <Card><p>Visibility: {props.cityData.visibility}km/h</p></Card>
-        </div>
-   
-        <div className={classes.block}>
-   <Card><p>Sunrise:{sunriseHour.toLocaleTimeString('en-US')} Sunset:{sunsetHour.toLocaleTimeString('en-US')}</p></Card>
-   <Card><p>Feels like: {feelsLikeTempToCelsius}&#x2103;</p></Card>
-   </div>
-   </div>
-  
+        <Card>
+          <p>Humidity:</p>
+          <div className={classes["img-wrapper"]}>
+            <img src={humidityIcon} alt="" />
+          </div>
 
-        
-        
+          <p>{props.cityData.main.humidity} %</p>
+        </Card>
+        <Card>
+          <div className="card-header">
+            <p>Sunrise:</p> <p>Sunset:</p>
+          </div>
+          <div className={`${classes["card-icon-wrapper"]} ${classes["sun-card"]}`}>
+            <div className={classes["img-wrapper"]}>
+              <img src={sunriseIcon} alt="" />
+            </div>
+            <div className={classes["img-wrapper"]}>
+              <img src={sunsetIcon} alt="" />
+            </div>
+          </div>
+          <p>
+            {sunriseHour.toLocaleTimeString("en-US")}{" "}
+            {sunsetHour.toLocaleTimeString("en-US")}
+          </p>
+        </Card>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-
-export default WeatherHighlihts
+export default WeatherHighlihts;
--- client/src/components/WeatherToday.js
+++ client/src/components/WeatherToday.js
@@ -7,7 +7,9 @@ const WeatherToday = (props) =>{
    const weatherImage= `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
   
    let tempToCelsius = Math.floor(props.cityData.main.temp - 273,15);
-
+ let minTempToCelsius = Math.floor(props.cityData.main.temp_min - 273,15);
+   let maxTempToCelsius = Math.floor(props.cityData.main.temp_max - 273,15);
+   let feelsLikeTempToCelsius = Math.floor(props.cityData.main.feels_like - 273,15);
    
    
     return(
@@ -16,11 +18,13 @@ const WeatherToday = (props) =>{
     <div className="weather-today-upper-section">
      <img src={weatherImage} alt="weather" />
      <p>{tempToCelsius} &#x2103;</p>
+     Feels like: {feelsLikeTempToCelsius}&#x2103;
      <p>Description: {props.cityData.weather[0].description}</p>
      </div>
      <div className="weather-today-down-section">
         <p>City: {props.cityData.name}</p>
-       
+        <p>Min: {minTempToCelsius}&#x2103; Max: {maxTempToCelsius}&#x2103; </p>
+    <p>Main: {props.cityData.weather[0].main}</p>
       
         </div>
         </div>
